eae29e343d66e4a6f6459a59265aab9459b3b6bd
shivam6497 <kdmboy6397@gmail.com>
2026-09-11 15:08:08 +0000

    dockerize the frontend

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone", // required for Docker
+  transpilePackages: ["@notify/types"], // monorepo packages
 };
 
 export default nextConfig;
